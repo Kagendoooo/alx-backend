b2595e68ce2769c1b9a8f2ef824b1ac6d060662b
Gendozzzz <sunflowermaina@gmail.com>
2024-11-18 21:54:22 +0300

    Basic cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Class BasicCache that inherits from BaseCaching
+Caching system doesn’t have limit
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve an item from the cache by key
+        """
+        return self.cache_data.get(key)
