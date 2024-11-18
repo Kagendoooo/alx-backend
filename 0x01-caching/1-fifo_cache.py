22fcc789f671920b925b6a5022e1b471d5b995d8
Gendozzzz <sunflowermaina@gmail.com>
2024-11-18 22:05:46 +0300

    FIFO

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+FIFO caching module
+"""
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class FIFOCache(BaseCaching):
+    """Caching system using FIFO algorithm"""
+    def __init__(self):
+        """Class initialization"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Add item to cache"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
+
+    def get(self, key):
+        """Retrieve an item"""
+        return self.cache_data.get(key, None)
